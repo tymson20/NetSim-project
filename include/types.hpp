38f36ae86126c45192972159eee80b4cfa6948dc
Mateusz Cierpik <mateuszcierpik@gmail.com>
2023-01-13 20:51:59 +0100

    nowe aliasy

--- include/types.hpp
+++ include/types.hpp
@@ -2,6 +2,11 @@
 #ifndef NETSIM_PROJECT_TYPES_HPP
 #define NETSIM_PROJECT_TYPES_HPP
 
+#include <functional>
+
 using ElementID = int;
+using Time = int;
+using TimeOffset = int;
+using ProbabilityGenerator = std::function<double()>;
 
 #endif //NETSIM_PROJECT_TYPES_HPP
