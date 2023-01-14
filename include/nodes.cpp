7bf83f7d6a4517f8418de251245c57ec2dee459f
Mateusz Cierpik <mateuszcierpik@gmail.com>
2023-01-14 18:06:23 +0100

    klasa PackageSender + Ramp (zaczęte)

--- include/nodes.cpp
+++ include/nodes.cpp
@@ -1,3 +1,27 @@
 
 
 #include "nodes.hpp"
+
+void PackageSender::send_package() {
+    if(bufor_){
+
+        receiver_preferences_ = *bufor_;
+        // !!!!
+        //
+
+        bufor_.reset();
+    }
+}
+
+void PackageSender::push_package(Package&& obj) {
+    if(!bufor_) {
+        bufor_ = std::move(obj);
+    }
+}
+
+void Ramp::deliver_goods(Time t){
+    if(t % di_ == 1){
+        Package package;
+        push_package(std::move(package));
+    }
+}
--- include/nodes.hpp
+++ include/nodes.hpp
@@ -3,6 +3,52 @@
 #define NETSIM_PROJECT_NODES_HPP
 
 
+#include <optional>
+#include <list>
+#include "package.hpp"
+
+class ReceiverPreferences {
+
+};
+
+class PackageSender {
+public:
+    PackageSender() = default;
+    PackageSender(PackageSender&&) = default;
+
+    ReceiverPreferences receiver_preferences_;
+
+    void send_package();
+    std::optional<Package>& get_sending_buffer() { return bufor_; }
+
+protected:
+    void push_package(Package&& obj);
+
+private:
+    std::optional<Package> bufor_;
+
+};
+
+
+class Ramp : public PackageSender {
+public:
+    Ramp(ElementID id, TimeOffset di) : id_(id), di_(di) {}
+
+    void deliver_goods(Time t);
+    ElementID get_id() const { return id_; }
+    TimeOffset get_delivery_interval() const { return di_; }
+
+private:
+    ElementID id_;
+    TimeOffset di_;
+
+};
+
+class Worker : public PackageSender {
+
+};
+
+
 
 
 #endif //NETSIM_PROJECT_NODES_HPP
