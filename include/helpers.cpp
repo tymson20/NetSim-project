7089e47f13a5c80bd9b7ca533716c00539d7b0c7
Mateusz Cierpik <mateuszcierpik@gmail.com>
2023-01-14 16:27:24 +0100

    nowe pliczki + helpers

---
+++ include/helpers.cpp
@@ -0,0 +1,16 @@
+
+
+#include "helpers.hpp"
+
+#include <cstdlib>
+#include <random>
+
+
+std::random_device rd;
+std::mt19937 rng(rd());
+
+double default_probability_generator() {
+    return std::generate_canonical<double, 10>(rng);
+}
+
+std::function<double()> probability_generator = default_probability_generator;
--- include/helpers.hpp
+++ include/helpers.hpp
@@ -1,6 +1,20 @@
 
-#ifndef NETSIM_HELPERS_HPP
-#define NETSIM_HELPERS_HPP
+#ifndef NETSIM_PROJECT_HELPERS_HPP
+#define NETSIM_PROJECT_HELPERS_HPP
 
 
-#endif //NETSIM_HELPERS_HPP
+#include <functional>
+#include <random>
+
+#include "types.hpp"
+
+extern std::random_device rd;
+extern std::mt19937 rng;
+
+extern double default_probability_generator();
+
+extern ProbabilityGenerator probability_generator;
+
+
+
+#endif //NETSIM_PROJECT_HELPERS_HPP
