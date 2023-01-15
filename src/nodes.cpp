ad074efb3f8cb2a90df434e54b763ab858e6fc97
Michał <cynarski60@gmail.com>
2023-01-15 17:46:05 +0100

    klasa worker i IPackageReceiver

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -5,7 +5,7 @@
 void PackageSender::send_package() {
     if(bufor_){
 
-        receiver_preferences_ = *bufor_;
+        //receiver_preferences_ = *bufor_;
         // !!!!
         //
 
@@ -23,5 +23,23 @@ void Ramp::deliver_goods(Time t){
     if(t % di_ == 1){
         Package package;
         push_package(std::move(package));
+    }
+}
+
+void Worker::do_work(Time t) {
+    if(pd_ == 1){
+        push_package(q_->pop());
+        stime_ = t;
+    }else {
+        if (!worker_buffor_.has_value()) {
+            if (!q_->empty()) {
+                worker_buffor_.emplace(q_->pop());
+                stime_ = t;
+            }
+        }
+        if (t - stime_ == pd_ - 1) {
+            push_package(std::move(worker_buffor_.value()));
+            worker_buffor_ = std::nullopt;
+        }
     }
 }
